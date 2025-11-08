f3807379c5120c412e0cff05f9f94ab5b5bdfc63
xBobrov <andy.beaver.vld@gmail.com>
2025-11-08 15:55:11 +1000

    init

---
+++ src/main/java/com/vodokanal/accounting/VodokanalApplication.java
@@ -0,0 +1,13 @@
+package com.vodokanal.accounting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodokanalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VodokanalApplication.class, args);
+    }
+
+}
