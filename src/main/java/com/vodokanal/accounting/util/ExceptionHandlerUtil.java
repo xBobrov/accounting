f96aa23ecb471612f475bb3453e34da9e8aa4419
xBobrov <andy.beaver.vld@gmail.com>
2025-12-12 11:14:28 +1000

    + logging upd

--- src/main/java/com/vodokanal/accounting/util/ExceptionHandlerUtil.java
+++ src/main/java/com/vodokanal/accounting/util/ExceptionHandlerUtil.java
@@ -13,32 +13,49 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @ControllerAdvice
 public class ExceptionHandlerUtil {
+    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<ErrorResponseDto> handleNoSuchElementException(NoSuchElementException e){
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDto> handleException(Exception e) {
 
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(LocalDateTime.now(),
                 "Bad Request", e.getMessage());
+        log.error("В процессе обработки произошла непредвиденная ошибка", e);
+
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponseDto> handleNoSuchElementException(NoSuchElementException e) {
+
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(LocalDateTime.now(),
+                "Bad Request", e.getMessage());
+        log.error("При записи в базу данных произошла ошибка", e);
 
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<ErrorResponseDto> handleConstraintViolationException(ConstraintViolationException e){
+    public ResponseEntity<ErrorResponseDto> handleConstraintViolationException(ConstraintViolationException e) {
 
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(LocalDateTime.now(),
                 "Bad Request", e.getMessage());
+        log.error("При записи в базу данных произошла ошибка", e);
 
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(HandlerMethodValidationException.class)
-    public ResponseEntity<ErrorResponseDto> handleHMVException(HandlerMethodValidationException e){
+    public ResponseEntity<ErrorResponseDto> handleHMVException(HandlerMethodValidationException e) {
 
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(LocalDateTime.now(),
                 "Bad Request", "В переданном списке содержаться ошибки");
+        log.error("При записи в базу данных произошла ошибка", e);
 
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
@@ -48,14 +65,15 @@ public class ExceptionHandlerUtil {
 
         ErrorResponseDto errorResponseDto = new ErrorResponseDto(LocalDateTime.now(),
                 "Bad Request", getErrorMessageList(e).toString());
+        log.error("При записи в базу данных произошла ошибка", e);
 
         return new ResponseEntity<>(errorResponseDto, HttpStatus.BAD_REQUEST);
     }
 
     private List<String> getErrorMessageList(MethodArgumentNotValidException e) {
 
-       return e.getBindingResult().getAllErrors().stream()
-               .map(DefaultMessageSourceResolvable::getDefaultMessage)
-               .toList();
+        return e.getBindingResult().getAllErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
     }
 }
--- src/main/java/com/vodokanal/accounting/util/RequestHandler.java
+++ src/main/java/com/vodokanal/accounting/util/RequestHandler.java
@@ -24,7 +24,7 @@ public class RequestHandler {
 
     @PostMapping("/account/add/list")
     public ResponseEntity<List<AccountDto>> addAccountList(@RequestBody @Valid List<AccountDto> accountDtoList) {
-        log.info("Called addAccountList with body {}", accountDtoList);
+        log.info("Вызван метод addAccountList с телом запроса: {}", accountDtoList);
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(accountService.addAccount(accountDtoList));
@@ -32,6 +32,8 @@ public class RequestHandler {
 
     @PostMapping("/account/add/single")
     public ResponseEntity<AccountDto> addAccount(@RequestBody @Valid AccountDto accountDto) {
+        log.info("Вызван метод addAccount с телом запроса: {}", accountDto);
+
         List<AccountDto> accountDtoList = accountService.addAccount(List.of(accountDto));
 
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -41,6 +43,8 @@ public class RequestHandler {
     @PatchMapping("/account/update/{id}")
     public ResponseEntity<AccountDto> updateAccount(@PathVariable long id,
                                                        @RequestBody @Valid AccountUpdateDto accountUpdateDto) {
+        log.info("Вызван метод updateAccount с телом запроса: {}", accountUpdateDto);
+
         return ResponseEntity.status(HttpStatus.OK).body(accountService.updateAccount(id, accountUpdateDto));
     }
 
